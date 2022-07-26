2715c230acb7a93db7732b486f687c317c8de26d
williamcruz <cruzmedien@icloud.com>
2022-07-26 13:39:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        double miles = SpeedConverter.toMilesPerHour(-5.6);
+        SpeedConverter.printConversion(miles);
+    }
+}
